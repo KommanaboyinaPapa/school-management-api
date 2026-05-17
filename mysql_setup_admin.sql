6f19724da8a5f61ed28b5d69eb7ff65f9f5b48e8
PAPA <papakommanaboyina@gmail.com>
2026-05-17 18:03:52 +0530

    Remove extra MySQL setup script

--- mysql_setup_admin.sql
+++
@@ -1,9 +0,0 @@
--- Run this as a privileged MySQL user (e.g., root) to create the app user.
-
-CREATE DATABASE IF NOT EXISTS school_db;
-
-CREATE USER IF NOT EXISTS 'admin'@'localhost' IDENTIFIED BY '123';
-
-GRANT ALL PRIVILEGES ON school_db.* TO 'admin'@'localhost';
-
-FLUSH PRIVILEGES;
